fad7597151a23ef8e41ab6e7417d964333b528f2
AlienBruhh <forsomerandompurpose@gmail.com>
2025-01-02 23:36:52 +0530

    Project Init(Code fixes5)

--- dist/chaina-portolio/server/angular-app-manifest.mjs
+++ dist/chaina-portolio/server/angular-app-manifest.mjs
@@ -20,10 +20,10 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 15401, hash: '4b29eb1f184980807cf3a4eb5e43a1e88dd4d382c386af829ac93fd5c09c912c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 8833, hash: '55a92cb717d343e84a78ce629ff5e27c4c949c8f1ed42659a5b7d17b7a0a68de', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'resume/index.html': {size: 18099, hash: 'ef109de98bb9417843572f49651eab97763d79bbaf1fe5585e859ab81e8792a8', text: () => import('./assets-chunks/resume_index_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 18099, hash: 'ef109de98bb9417843572f49651eab97763d79bbaf1fe5585e859ab81e8792a8', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 15401, hash: 'b31b90cf89026aaca4035d52f5ecaf26b8d4bc849ac25ece1bb2da8b02239135', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 8833, hash: '205517eb2e7e73936b64edafd65c2fb250d50b5b46109794c52070947aa432b7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'resume/index.html': {size: 18100, hash: 'cedbb6318c89c083ace170ce9b224a279dbcc2e7abd1521076f3a494e1af695b', text: () => import('./assets-chunks/resume_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 18100, hash: 'cedbb6318c89c083ace170ce9b224a279dbcc2e7abd1521076f3a494e1af695b', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
     'styles-UNI6UW42.css': {size: 7154, hash: '8tr+6X9Fryc', text: () => import('./assets-chunks/styles-UNI6UW42_css.mjs').then(m => m.default)}
   },
 };
